b196f8e110fa61cda76a9d8c0015b72f147b7988
Sofia Acosta <sofiaacostarascon@gmail.com>
2021-09-19 14:05:40 -0500

    test and write our reducer's Default State
    Co-authored-by: Saoud Rana <saoud@saoud.dev>

--- src/reducers/headlines-reducer.js
+++ src/reducers/headlines-reducer.js
@@ -1,9 +1,28 @@
-const defaultState = {
+import * as c from './../actions/ActionTypes';
+
+let initialState = {
   isLoading: false,
   headlines: [],
   error: null
 }
 
-export default (state = defaultState, action) => {
-  return state;
+export default (state = initialState, action) => {
+  switch (action.type) {
+    case c.REQUEST_HEADLINES:
+      return Object.assign({}, state, {
+        isLoading: true
+      });
+    case c.GET_HEADLINES_SUCCESS:
+      return Object.assign({}, state, {
+        isLoading: false,
+        headlines: action.headlines
+      });
+    case c.GET_HEADLINES_FAILURE:
+      return Object.assign({}, state, {
+        isLoading: false,
+        error: action.error
+      });  
+    default:
+      return state;
+    }
 };
